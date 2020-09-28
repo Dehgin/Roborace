0e239173db39038c7e38f2742769a2a361ad1088
Jaime McIntosh <mcintosj@mail.gvsu.edu>
2020-09-28 15:07:19 -0400

    Update Gitlab2.5.ino
    added void setup ()

--- Gitlab2.5/Gitlab2.5.ino
+++ Gitlab2.5/Gitlab2.5.ino
@@ -1,8 +1,13 @@
 Carson edit 1
+void setup () {
+  
+
 printf("Hello World"); // Alen edit 1
 Carson edit 2
 int a = 180; // Alen edit 2
-Jayme edit 1 
+Jaime edit 1 
 Dillon edit 1
-Jayme edit 2
+Jaime edit 2
 Dillon edit 2 
+
+}
